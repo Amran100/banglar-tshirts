b1f5aea0ee00fc5d6d41c6ce50726f8b7b923345
unknown <you@example.com>
2023-04-07 03:21:10 +0600

    added context api

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './Gradnpa.css'
 
+export const RingContext = createContext('GOLD')
+export const MoneyContext = createContext(0)
+
 const Grandpa = () => {
     const ring = 'diamond'
+    const [money , setMoney] = useState(0)
+    
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
+            <p>Has money : {money}</p>
+           <MoneyContext.Provider value={[money , setMoney] }>
+           <RingContext.Provider value = 'Gold Ring'>
             <section className='flex'>
             <Father ring = {ring}></Father>
             <Uncle></Uncle>
             <Aunty ring = {ring}></Aunty>
             </section>
+            </RingContext.Provider>
+           </MoneyContext.Provider>
         </div>
     );
 };
--- src/components/Uncle/Uncle.jsx
+++ src/components/Uncle/Uncle.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Cousin from '../Cousin/Cousin';
+import { MoneyContext } from '../Grandpa/Grandpa';
 
 const Uncle = () => {
+    const [money , setMoney]= useContext(MoneyContext)
     return (
         <div>
            <h2>Uncle</h2> 
+           <p>Grandpa Money : {money}</p>
+           <button onClick={()=> setMoney(money + 100000)}>Send 1 lak</button>
            <section className='flex'>
             <Cousin>Nibir</Cousin>
             <Cousin>HoiChoi</Cousin>
